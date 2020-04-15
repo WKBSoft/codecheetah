05666c7ecffd135cb69f7a638f2c20b15d67b827
root <root@ip-172-31-20-208.us-east-2.compute.internal>
2020-04-15 21:24:29 +0000

    auto commit

--- temp.py
+++ temp.py
@@ -1,4 +1,12 @@
 import os
-for root, dirs, files in os.walk(".", topdown=False):
-   for name in dirs:
-      print(name)
+dirs_list = []
+for root, dirs, files in os.walk("/home/ec2-user/repos", topdown=False):
+        for name in dirs:
+            my_dir = os.path.join(root,name)
+            my_dir_list = my_dir.split("/")
+            del my_dir_list[0:4]
+	    if len(my_dir_list) > 1:
+                if my_dir_list[1] != ".git":
+            	    my_dir = "/".join(my_dir_list)
+            	    dirs_list.append(my_dir)
+print dirs_list
