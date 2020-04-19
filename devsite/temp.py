86d053ef6747695cbe4e6b41be24c1827cd2e3d4
root <root@ip-172-31-20-208.us-east-2.compute.internal>
2020-04-19 23:01:51 +0000

    auto commit

--- devsite/temp.py
+++
@@ -1,6 +0,0 @@
-import sys
-sys.path.insert(0, '/home/ec2-user/devsite/devsite/localviews/')
-import content_gen
-
-repo_accordion = content_gen.path_accordion("/home/ec2-user/repos")
-print repo_accordion
--- temp.py
+++ temp.py
@@ -1,12 +1,3 @@
-import os
-dirs_list = []
-for root, dirs, files in os.walk("/home/ec2-user/repos", topdown=False):
-        for name in dirs:
-            my_dir = os.path.join(root,name)
-            my_dir_list = my_dir.split("/")
-            del my_dir_list[0:4]
-	    if len(my_dir_list) > 1:
-                if my_dir_list[1] != ".git":
-            	    my_dir = "/".join(my_dir_list)
-            	    dirs_list.append(my_dir)
-print dirs_list
+import requests
+x = requests.get("http://localhost:5000")
+print x
