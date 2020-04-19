4c97aa3c21cec1809731e740e94a25971e7c5fa8
root <root@ip-172-31-20-208.us-east-2.compute.internal>
2020-04-19 20:20:08 +0000

    auto commit

--- devsite/localviews/content_gen.py
+++ devsite/localviews/content_gen.py
@@ -80,9 +80,9 @@ def write_html(tree):
     level = list(filter(lambda x: len(x.split("/")) == i,tree))
     if html == "":
         for x in level:
-            loc_loc_html = accordian.replace("collapse_id","collapse_"+x)
-            loc_loc_html = accordian.replace("accordian_id","accordian_"+x)
-            loc_html = accordian.replace("<!-- collapse link -->",x)
+            loc_html = accordian.replace("collapse_id","collapse_"+x)
+            loc_html = loc_html.replace("accordian_id","accordian_"+x)
+            loc_html = loc_html.replace("<!-- collapse link -->",x)
             loc_html = loc_html.replace("<!-- collapse content -->","<!-- collapse content "+x+"-->")
             html += loc_html
     while i<5:
@@ -99,7 +99,7 @@ def write_html(tree):
                     print y
                     dir_id = x+"/"+y
                     loc_loc_html = accordian.replace("collapse_id","collapse_"+dir_id)
-                    loc_loc_html = accordian.replace("accordian_id","accordian_"+dir_id)
+                    loc_loc_html = loc_loc_html.replace("accordian_id","accordian_"+dir_id)
                     loc_loc_html = loc_loc_html.replace("<!-- collapse link -->",y)
                     loc_loc_html = loc_loc_html.replace("<!-- collapse content -->","<!-- collapse content "+dir_id+"-->")
                     loc_html += loc_loc_html
