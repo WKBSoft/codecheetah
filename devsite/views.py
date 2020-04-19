d7df2aa5849c4be910283a207bf308b3ae448aaa
root <root@ip-172-31-20-208.us-east-2.compute.internal>
2020-04-19 20:30:56 +0000

    auto commit

--- devsite/views.py
+++ devsite/views.py
@@ -17,19 +17,8 @@ def get_code_format(code_type):
 
 def home(request):
     dirs_list = []
-    for root, dirs, files in os.walk("/home/ec2-user/repos", topdown=False):
-        for name in dirs:
-            my_dir = os.path.join(root,name)
-            my_dir_list = my_dir.split("/")
-            del my_dir_list[0:4]
-	    if len(my_dir_list) > 1:
-                if my_dir_list[1] != ".git":
-            	    my_dir = "/".join(my_dir_list)
-            	    dirs_list.append(my_dir)
-            else:
-                my_dir = "/".join(my_dir_list)
-                dirs_list.append(my_dir)
-    return(render(request,'home.html',{'active_page':'code_page','repo_tree':dirs_list}))
+    repo_accordion = content_gen.path_accordion("/home/ec2-user/repos")
+    return(render(request,'home.html',{'active_page':'code_page','repo_accordion':repo_accordion}))
 
 def openfile(request):
     code_loc = request.GET['q']
