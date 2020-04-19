12c292ce4ee4109db557a10731829469a03cff4d
root <root@ip-172-31-20-208.us-east-2.compute.internal>
2020-04-19 17:33:16 +0000

    auto commit

--- devsite/localviews/content_gen.py
+++ devsite/localviews/content_gen.py
@@ -90,10 +90,12 @@ test_list = [
         ['A','X','C','D'],
 	    ['A','B','Y','D']
     ],
+    [
         ['A','F'],
         ['A','B','Y','E'],
         ['A','B','C','D','E'],
         ['A','B','C','D','F']
+    ]
 ]
 #my_path_list = list_subpaths("/home/ec2-user/repos/devsite")
 #print my_path_list
