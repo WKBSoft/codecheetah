fca93a91d345eab49b4edd96eb49c6aa72bc66f5
root <root@ip-172-31-20-208.us-east-2.compute.internal>
2020-04-19 20:57:07 +0000

    auto commit

--- devsite/temp.py
+++ devsite/temp.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.insert(0, '/home/ec2-user/repos/devsite/devsite/localviews/')
+sys.path.insert(0, '/home/ec2-user/devsite/devsite/localviews/')
 import content_gen
 
 repo_accordion = content_gen.path_accordion("/home/ec2-user/repos")
