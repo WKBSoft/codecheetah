430d1ada9109cfd57ad5e25083a6bdc6c8cf3b68
Wesley Belleman <bellemanwesley@gmail.com>
2020-04-27 06:32:38 +0000

    auto commit

--- temp.py
+++ temp.py
@@ -3,4 +3,4 @@ sys.path.insert(0, '/home/ec2-user/repos/devsite/devsite/localviews/')
 import content_gen
 import deploy_code
 
-print(content_gen.list_subpaths('home/ec2-user/repos/vitamova/s3_files'))
+print(content_gen.list_subpaths('/home/ec2-user/repos/vitamova/s3_files'))
