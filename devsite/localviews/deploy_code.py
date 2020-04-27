9428e2fa6e5de3e764eade61c2d06c1fae73ce5a
Wesley Belleman <bellemanwesley@gmail.com>
2020-04-27 06:18:22 +0000

    auto commit

--- devsite/localviews/deploy_code.py
+++ devsite/localviews/deploy_code.py
@@ -1,7 +1,13 @@
 import requests
 import boto3
+from os import path
 
 def deploy(request):
-    deploy_result = requests.get('http://localhost:5000')
+    dest_ip = request.POST['ip_addr']
+    deploy_result = requests.get('http://'+des_ip+':5000')
+    repo = request.POST['default_save'].split("/")[0]
+    if path.isdir("/home/ec2-user/repos/"+repo+"/s3_files"):
+        s3_bucket = request.POST['s3_bucket']
+        
     return deploy_result
     
