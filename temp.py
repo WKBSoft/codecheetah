40d089a5ba302ce5ab128b692dd0d8e79bd626f0
Wesley Belleman <bellemanwesley@gmail.com>
2020-04-27 00:16:55 +0000

    auto commit

--- temp.py
+++ temp.py
@@ -1,8 +1,1 @@
-import sys
-sys.path.insert(0, '/home/ec2-user/devsite/devsite/localviews/')
-import content_gen
-
-a = "b" "c"
-available_files = content_gen.list_contents("/home/ec2-user/repos")[1]
-print available_files
-print "../keys/django_key.txt" in available_files
+supid
