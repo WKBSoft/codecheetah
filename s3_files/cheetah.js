439e094c3048a88554992f52c89187a264b654fa
Wesley Belleman <bellemanwesley@gmail.com>
2020-04-28 19:32:06 +0000

    auto commit

--- s3_files/cheetah.js
+++ s3_files/cheetah.js
@@ -21,7 +21,7 @@ function load_response(url, cFunction) {
   send_data = send_data.concat("&csrfmiddlewaretoken=".concat(csrf_token_value));
   xhttp.open("POST", url, true);
   xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  document.getElementById("run_code_result").innerHTML = "<p>".concat(send_data).concat("</p>");
+  document.getElementById("run_code_result").innerHTML = send_data;
   //xhttp.send(send_data);
 }
   
