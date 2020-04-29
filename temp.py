de3c4f9b457c0dbd21976a26d79a819688abfb73
Wesley Belleman <bellemanwesley@gmail.com>
2020-04-29 03:53:16 +0000

    auto commit

--- temp.py
+++ temp.py
@@ -1,6 +1,30 @@
-import sys
-sys.path.insert(0, '/home/ec2-user/repos/devsite/devsite/localviews/')
-import content_gen
-import deploy_code
+import os
+request = {
+    'script':"#test script",
+    'q':"devsite/temp_2.py"
+}
 
-print(content_gen.list_subpaths('/home/ec2-user/repos/vitamova/s3_files'))
+
+
+
+def savefile(request):
+    my_code = request['script']
+    code_loc = request['q']
+    code_loc_list = code_loc.split('.')
+    code_type = code_loc_list[len(code_loc_list)-1]
+    repo_loc = '/home/ec2-user/repos/'+code_loc.split('/')[0]
+    if True:
+        #try:
+        with open("/home/ec2-user/repos/"+code_loc,'w+') as f:
+            f.write(my_code)
+        os.system('git -C ' +repo_loc+ ' add .')
+        os.system('git -C ' +repo_loc+ " commit -m 'auto commit'")
+        os.system('git -C ' +repo_loc+ ' push origin master')
+        response = "Success"
+        #except:
+            #response = "Failure"
+    else:
+        response = "Access denied"
+    return(response)
+
+print(savefile(request))
